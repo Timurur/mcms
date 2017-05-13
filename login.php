735607cfddd968d1875ba973904e4b2f3eaa7a57
Timurur <Teemurcka@gmail.com>
2017-05-13 15:30:01 +0300

    HOT FIXES!

--- login.php
+++ login.php
@@ -1,10 +1,3 @@
-
-/**
- * Created by PhpStorm.
- * User: Тимур
- * Date: 13.05.2017
- * Time: 15:09
- */
 <?php
 //if (isset($_POST['login'])&&isset($_POST['password'])) {
 	//echo(login($_POST['login'],$_POST['password']));
@@ -26,4 +19,4 @@ function login($login,$psw){
     else{
         return "Vsehuevo";}
 }
- ?>
+?>
